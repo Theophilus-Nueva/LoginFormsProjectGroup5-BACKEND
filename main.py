8bd32e50ed4e57c4f150a648802cce20f6014cef
Theophilus <theophilusnueva30@icloud.com>
2026-05-11 11:07:28 +0800

    Added live frontend to CORS VIP list

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import login
+import signup  
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://loginformsprojectgroup5-production.up.railway.app"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(login.router)
+app.include_router(signup.router) 
 
 @app.get("/")
 def read_root():
